1176836f6403e01fb9c9f61a90999a03fa5631dc
Christian Cruz <christian.cruz92@hotmail.com>
2024-06-24 06:06:48 -0400

    added the shell for the Todo, TodoList, and NewTodoForm components

---
+++ src/Todo.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+const Todo = () => {
+
+}
+
+export default Todo;
---
+++ src/TodoList.js
@@ -0,0 +1,9 @@
+import react, {useState} from "react";
+import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm"
+
+const TodoList = () => {
+
+}
+
+export default TodoList;
